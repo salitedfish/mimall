07f425763e05323f0d6af56690c1badef68e3b07
葛晓康 <1525185228@qq.com>
2020-11-28 13:52:52 +0800

    目录建立

---
+++ src/pages/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav-header><nav-header>
+    <router-view><router-view>
+    <nav-footer><nav-footer>
+  </div>
+</template>
+
+<script>
+import NavHeader from "../../components/navHeader/NavHeader.vue";
+import NavFooter from "../../componentonents/NavFooter/NavFooter.vue";
+
+export default {
+  components: { NavFooter, NavHeader },
+  name: "home",
+};
+</script>
+    NavFooter
+
+<style>
+</style>
